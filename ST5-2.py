2c75cdcaffcf3ce6bbb1b47a9c1134b618241f54
Kane1z <pdanielcp6@gmail.com>
2024-11-10 22:14:31 +0000

    ST2 - maior numero

---
+++ ST5-2.py
@@ -0,0 +1,15 @@
+def achou(numero):
+    maior = 0
+    for digito in str(numero):
+        '''vou tornar o numero em uma string e assim posso verificar 1 por 1
+        print(int(digito))
+        '''
+        if int(digito) > maior:
+            maior = int(digito)
+    return maior
+
+
+numero = int(input("Digite um número inteiro: "))
+
+
+print("O maior algarismo em",achou(numero))
